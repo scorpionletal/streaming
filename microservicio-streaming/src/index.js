2699061095e1afb31802411d2354972d70484b60
scorpions <uni.ramos.telecomun@gmail.com>
2020-05-03 07:47:24 -0500

    con puerto 3389

--- microservicio-streaming/src/index.js
+++ microservicio-streaming/src/index.js
@@ -3,7 +3,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3389;
 
 var publicDir = `${__dirname}/public`;
 
