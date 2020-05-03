f9b9e1cf63690e0926849ce41afc48d4a3694f3e
scorpions <uni.ramos.telecomun@gmail.com>
2020-05-03 06:22:42 -0500

    proyecto streaming

---
+++ microservicio-streaming/src/index.js
@@ -0,0 +1,27 @@
+'user strict'
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var port = process.env.PORT || 3000;
+
+var publicDir = `${__dirname}/public`;
+
+http.listen(port, ()=>{
+    console.log('iniciando express y Socket.io en localhost:%d',port);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(`${publicDir}/client.html`)
+});
+
+app.get('/streaming', (req, res) => {
+    res.sendFile(`${publicDir}/server.html`);
+});
+
+io.on('connection',(socket)=>{
+    socket.on('streaming', (image)=>{
+        io.emit('play stream', image);
+       
+    });
+});
